987f7c1f53106590d22390f53c28cd6d2eac7e0e
Emilioo0 <luis310102@gmail.com>
2022-11-08 19:51:52 -0600

    update

---
+++ Nodo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BlackRedTree
+{
+
+    public enum ArbolColor
+    {
+        rojo,
+        negro
+    };
+
+    public class Nodo
+    {
+
+        public Nodo izquierdo;
+        public Nodo derecho;
+        public Nodo padre;
+        public int dato;
+        public ArbolColor color;
+        public int nivel;
+
+        public Nodo(int dato, ArbolColor color, int nivel)
+        {
+            this.dato = dato;
+            this.color = color;
+            this.nivel = nivel;
+        }
+
+    }
+}
