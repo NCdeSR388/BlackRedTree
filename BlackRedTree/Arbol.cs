d6d1c316e4098bb79aac9bb73a2fbe6963f78d23
YANA021 <na21011@ues.edu.sv>
2022-11-10 11:42:16 -0600

    Adicion de metodo grafico para nodos almacenados en arbol

--- BlackRedTree/Arbol.cs
+++ BlackRedTree/Arbol.cs
@@ -1,21 +1,28 @@
-﻿using SharpDX.Direct2D1.Effects;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace BlackRedTree
 {
     public class Arbol
     {
         public Nodo raiz;
-        int nivelMax;
         public string inorden = "Inorden: ";
         public string preorden = "Preorden: ";
         public string posorden = "Posorden: ";
+        //parametros graficos para posiciones de nodo
+        int distanceX = 20;
+        int distanceY = 55;
+        int diameter = 50;
+        int X_raiz = 600, Y_raiz = 20;
+        int alturaArbol;
 
         public Arbol()
         {
             raiz = null;
-            nivelMax = 0;
+            alturaArbol = 0;
         }
 
         // Insercion de datos
@@ -63,6 +70,48 @@ namespace BlackRedTree
                 posorden += n.clave + ", ";
             }
             return posorden;
+        }
+        private void posicion_nodo()
+        {
+            alturaArbol =raiz.altura(0,0);
+            raiz.nodoX = X_raiz;
+            raiz.nodoY = Y_raiz;
+            raiz.diameter = diameter;
+            if (raiz.izquierdo != null)
+            {
+                raiz.izquierdo.XY_position(X_raiz - distanceX - (alturaArbol*diameter)/2, Y_raiz + distanceY, distanceY, distanceX, diameter, alturaArbol/2);
+            }
+            if (raiz.derecho != null)
+            {
+                raiz.derecho.XY_position(X_raiz + distanceX + (alturaArbol * diameter)/2, Y_raiz + distanceY, distanceY, distanceX, diameter, alturaArbol/2);
+            }
+        }
+        public void dibujar_arbol(GameTime gameTime, Texture2D[] Textures, SpriteBatch _spriteBatch, SpriteFont Font)
+        {
+            
+            posicion_nodo();
+            if (raiz.izquierdo != null)
+            {
+                _spriteBatch.Begin();
+                Rectangle rectLine = new Rectangle(X_raiz - (diameter / 2)- (alturaArbol * diameter) / 3, Y_raiz + (diameter / 2), distanceX + (alturaArbol * diameter) / 2, distanceY);
+                _spriteBatch.Draw(Textures[1], rectLine, Color.Gray);
+                _spriteBatch.End();
+                raiz.izquierdo.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY,alturaArbol/2);
+            }
+            if (raiz.derecho != null)
+            {
+                _spriteBatch.Begin();
+                Rectangle rectLine = new Rectangle(X_raiz + (diameter/2), Y_raiz + (diameter / 2), distanceX + (alturaArbol * diameter) / 2, distanceY);
+                _spriteBatch.Draw(Textures[2], rectLine, Color.Gray);
+                _spriteBatch.End();
+                raiz.derecho.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY,alturaArbol/2);
+            }
+            _spriteBatch.Begin();
+            Rectangle rect_nodo = new Rectangle(X_raiz, Y_raiz, diameter, diameter);
+            _spriteBatch.Draw(Textures[0], rect_nodo, Color.Gray);
+            Vector2 VectorClave = new Vector2(X_raiz + (diameter / 4), Y_raiz + (diameter / 3));
+            _spriteBatch.DrawString(Font, raiz.clave.ToString(), VectorClave, Color.Black);
+            _spriteBatch.End();
         }
     }
 }
--- BlackRedTree/Nodo.cs
+++ BlackRedTree/Nodo.cs
@@ -1,14 +1,11 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Windows.Forms;
 
 namespace BlackRedTree
 {
 
-    public enum ArbolColor
-    {
-        rojo,
-        negro
-    };
 
     public class Nodo
     {
@@ -18,6 +15,9 @@ namespace BlackRedTree
         public Nodo derecho;
         public int color;
         public int nivel;
+
+        //Variables para caracteristicas graficas del nodo
+        public int nodoX, nodoY, diameter;
 
         public Nodo()
         {
@@ -33,7 +33,6 @@ namespace BlackRedTree
                 if (derecho == null)
                 {
                     derecho = new_nodo;
-                    MessageBox.Show(new_nodo.clave.ToString() + "hijo derecho de " + clave.ToString());
                 }
                 else
                 {
@@ -46,7 +45,6 @@ namespace BlackRedTree
                 if (izquierdo == null)
                 {
                     izquierdo = new_nodo;
-                    MessageBox.Show(new_nodo.clave.ToString() + "hijo izquierdo de " + clave.ToString());
                 }
                 else
                 {
@@ -59,7 +57,89 @@ namespace BlackRedTree
                 return false;
             }
         }
+        public void XY_position(int _X_nodo, int _Y_nodo, int distanceY, int distanceX, int _diameter, int alturaArbol)
+        {
+            nodoX = _X_nodo;
+            nodoY = _Y_nodo;
+            diameter = _diameter;
+            if (izquierdo != null)
+            {
+                izquierdo.XY_position(_X_nodo - distanceX - (alturaArbol * diameter) / 2, _Y_nodo + distanceY, distanceY, distanceX, _diameter, alturaArbol);
+            }
+            if (derecho != null)
+            {
+                derecho.XY_position(_X_nodo + distanceX + (alturaArbol * diameter) / 2, _Y_nodo + distanceY, distanceY, distanceX, _diameter, alturaArbol);
+            }
 
+        }
+        public void Draw_nodo(GameTime gameTime, Texture2D[] Textures, SpriteBatch _spriteBatch, SpriteFont Font, int distanceX, int distanceY, int alturaArbol)
+        {
+            
+            if(derecho != null)
+            {
+                _spriteBatch.Begin();
+                Rectangle rectLine = new Rectangle(nodoX + (diameter / 2), nodoY + (diameter / 2), distanceX + (alturaArbol * diameter) / 2, distanceY);
+                _spriteBatch.Draw(Textures[2], rectLine, Color.Gray);
+                _spriteBatch.End();
+                derecho.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY, alturaArbol);
+            }
+            if(izquierdo != null)
+            {
+                _spriteBatch.Begin();
+                Rectangle rectLine = new Rectangle(nodoX - (diameter / 2) - (alturaArbol * diameter) / 3, nodoY + (diameter / 2), distanceX + (alturaArbol * diameter) / 2, distanceY);
+                _spriteBatch.Draw(Textures[1], rectLine, Color.Gray);
+                _spriteBatch.End();
+                izquierdo.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY, alturaArbol);
+            }
+            _spriteBatch.Begin();
+            Rectangle rect_nodo = new Rectangle(nodoX, nodoY, diameter, diameter);
+            _spriteBatch.Draw(Textures[0], rect_nodo, Color.Gray);
+            Vector2 VectorClave = new Vector2(nodoX+(diameter/4), nodoY+(diameter/3)); 
+            _spriteBatch.DrawString(Font, clave.ToString(), VectorClave, Color.Black) ;
+            _spriteBatch.End();
+        }
+        public int altura(int altura_actual, int MayorNivel)
+        {
+            altura_actual++;
+            if(MayorNivel < altura_actual)
+            {
+                MayorNivel = altura_actual;
+            }
+            if(izquierdo != null)
+            {
+                MayorNivel = izquierdo.altura(altura_actual,MayorNivel);
+            }
+            if(derecho != null)
+            {
+                MayorNivel = derecho.altura(altura_actual, MayorNivel);
+            }
+            return MayorNivel;
+        }
+        /*
+         no es lo que queria pero devuelve el numero de nodos xd
+        public int altura(int altura_actual)
+        {
+            altura_actual++;
+            int a1 = 0 ;
+            if(derecho != null)
+            {
+                a1 = derecho.altura(altura_actual);
+            }
+            if(a1 > altura_actual)
+            {
+                altura_actual = a1;
+            }
+            if(izquierdo != null)
+            {
+                a1 = izquierdo.altura(altura_actual);
+            }
+            if (a1 > altura_actual)
+            {
+                altura_actual = a1;
+            }
+            return altura_actual;
+        }
+         */
 
     }
 }
