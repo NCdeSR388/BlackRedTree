52d81a04950b5a603721d7267524c12bb107314c
CesarDL <lovocesar29@gmail.com>
2022-11-09 19:06:22 -0600

    Recorrido Inorden, Posorden y Preorden

--- BlackRedTree/Arbol.cs
+++ BlackRedTree/Arbol.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SharpDX.Direct2D1.Effects;
+using System;
+using System.Collections.Generic;
 
 namespace BlackRedTree
 {
@@ -6,6 +8,9 @@ namespace BlackRedTree
     {
         public Nodo raiz;
         int nivelMax;
+        public string inorden = "Inorden: ";
+        public string preorden = "Preorden: ";
+        public string posorden = "Posorden: ";
 
         public Arbol()
         {
@@ -25,6 +30,39 @@ namespace BlackRedTree
             {
                 raiz.insertar(new_nodo);
             }
+        }
+
+        public string Inorden(Nodo n)
+        {
+            if (n != null)
+            {
+                Inorden(n.izquierdo);
+                inorden += n.clave + ", ";
+                Inorden(n.derecho);
+            }
+            return inorden;
+        }
+
+        public string Preorden(Nodo n)
+        {
+            if(n != null)
+            {
+                preorden += n.clave + ", ";
+                Preorden(n.izquierdo);
+                Preorden(n.derecho);
+            }
+            return preorden;
+        }
+
+        public string Posorden(Nodo n)
+        {
+            if (n != null)
+            {
+                Posorden(n.izquierdo);
+                Posorden(n.derecho);
+                posorden += n.clave + ", ";
+            }
+            return posorden;
         }
     }
 }
