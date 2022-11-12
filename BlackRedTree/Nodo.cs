f243a2751908624bb33ec46490d88044ffeea4c7
NCdeSR388 <cs21027@ues.edu.sv>
2022-11-11 21:30:28 -0600

    Reestructuracion de el metodo Dibujar previamente añadido

--- BlackRedTree/Nodo.cs
+++ BlackRedTree/Nodo.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
-using System.Windows.Forms;
 
 namespace BlackRedTree
 {
@@ -20,18 +19,19 @@ namespace BlackRedTree
         public int nivel;
         public int FacrorEquilibrio;
         public int alturaNodo;
+        private double compenzacion = 1.2;
         //Variables para ordenamiento del nodo
 
         public Nodo()
         {
             this.clave = rand.Next(1000);
-            color = Color.Red;
+            color = Color.DarkRed;
 
         }
         public Nodo(int clave)
         {
             this.clave = clave;
-            color = Color.Red;
+            color = Color.DarkRed;
 
         }
         public Nodo(int clave, Color color)
@@ -72,112 +72,104 @@ namespace BlackRedTree
                 return false;
             }
         }
-        public void XY_position(int _X_nodo, int _Y_nodo, int distanceY, int distanceX, int _diameter, int alturaArbol)
+        public int AlturaIzquierda()
+        {
+            int contador = 1;
+            if (izquierdo != null)
+            {
+                contador = contador + izquierdo.AlturaIzquierda();
+            }
+            return contador;
+        }
+        public int AlturaDerecha()
+        {
+            int contador = 1;
+            if (derecho != null)
+            {
+                contador = contador + derecho.AlturaDerecha();
+            }
+            return contador;
+        }
+        public void XY_position(int _X_nodo, int _Y_nodo, int distanceY, int distanceX, int _diameter)
         {
             nodoX = _X_nodo;
             nodoY = _Y_nodo;
             diameter = _diameter;
             if (izquierdo != null)
             {
-                izquierdo.XY_position(_X_nodo - distanceX - (alturaArbol * diameter) / 2, _Y_nodo + distanceY, distanceY, distanceX, _diameter, alturaArbol);
+                izquierdo.XY_position(Convert.ToInt32(_X_nodo - (distanceX * izquierdo.AlturaDerecha()) * compenzacion), _Y_nodo + distanceY, distanceY, distanceX, _diameter);
             }
             if (derecho != null)
             {
-                derecho.XY_position(_X_nodo + distanceX + (alturaArbol * diameter) / 2, _Y_nodo + distanceY, distanceY, distanceX, _diameter, alturaArbol);
+                derecho.XY_position(Convert.ToInt32(_X_nodo + (distanceX * derecho.AlturaIzquierda()) * compenzacion), _Y_nodo + distanceY, distanceY, distanceX, _diameter);
             }
 
         }
-        public void Draw_nodo(GameTime gameTime, Texture2D[] Textures, SpriteBatch _spriteBatch, SpriteFont Font, int distanceX, int distanceY, int alturaArbol)
+        public void Draw_nodo(GameTime gameTime, Texture2D[] Textures, SpriteBatch _spriteBatch, SpriteFont Font, int distanceX, int distanceY)
         {
-            
-            if(derecho != null)
+            //Dibujo de los enlaces entre los nodos
+            if (derecho != null)
             {
                 _spriteBatch.Begin();
-                Rectangle rectLine = new Rectangle(nodoX + (diameter / 2), nodoY + (diameter / 2), distanceX + (alturaArbol * diameter) / 2, distanceY);
+                Rectangle rectLine = new Rectangle(nodoX + (diameter / 2), nodoY + (diameter / 2), Convert.ToInt32(distanceX * derecho.AlturaIzquierda() * compenzacion), distanceY);
                 _spriteBatch.Draw(Textures[2], rectLine, Color.Gray);
                 _spriteBatch.End();
-                derecho.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY, alturaArbol);
+                derecho.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY);
             }
-            if(izquierdo != null)
+            if (izquierdo != null)
             {
                 _spriteBatch.Begin();
-                Rectangle rectLine = new Rectangle(nodoX - (diameter / 2) - (alturaArbol * diameter) / 3, nodoY + (diameter / 2), distanceX + (alturaArbol * diameter) / 2, distanceY);
+                Rectangle rectLine = new Rectangle(Convert.ToInt32(nodoX + (diameter / 2) - (distanceX * izquierdo.AlturaDerecha() * compenzacion)), nodoY + (diameter / 2), Convert.ToInt32(distanceX * izquierdo.AlturaDerecha() * compenzacion), distanceY);
                 _spriteBatch.Draw(Textures[1], rectLine, Color.Gray);
                 _spriteBatch.End();
-                izquierdo.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY, alturaArbol);
+                izquierdo.Draw_nodo(gameTime, Textures, _spriteBatch, Font, distanceX, distanceY);
             }
+            //Dibujo de el cuerpo de nodo y clave
             _spriteBatch.Begin();
             Rectangle rect_nodo = new Rectangle(nodoX, nodoY, diameter, diameter);
             _spriteBatch.Draw(Textures[0], rect_nodo, color);
-            Vector2 VectorClave = new Vector2(nodoX+(diameter/4), nodoY+(diameter/3)); 
-            _spriteBatch.DrawString(Font, clave.ToString(), VectorClave, Color.Black) ;
+            Vector2 VectorClave = new Vector2(nodoX + (diameter / 4), nodoY + (diameter / 3));
+            _spriteBatch.DrawString(Font, clave.ToString(), VectorClave, Color.Black);
             _spriteBatch.End();
         }
         public int altura(int altura_actual, int MayorNivel)
         {
             altura_actual++;
             this.alturaNodo = altura_actual;
-            if(MayorNivel < altura_actual)
+            if (MayorNivel < altura_actual)
             {
                 MayorNivel = altura_actual;
             }
-            if(izquierdo != null)
+            if (izquierdo != null)
             {
-                MayorNivel = izquierdo.altura(altura_actual,MayorNivel);
+                MayorNivel = izquierdo.altura(altura_actual, MayorNivel);
             }
-            if(derecho != null)
+            if (derecho != null)
             {
                 MayorNivel = derecho.altura(altura_actual, MayorNivel);
             }
             return MayorNivel;
         }
-
-        public void CalcularFactorEq()
-        {
-            FacrorEquilibrio = 0;
-            if(izquierdo != null)
-            {
-                FacrorEquilibrio--;
-                if(izquierdo.izquierdo != null || izquierdo.derecho != null)
-                {
-                    FacrorEquilibrio--;
-                }
-                izquierdo.CalcularFactorEq();
-            }
-            if(derecho != null)
-            {
-                FacrorEquilibrio++;                
-                if(derecho.izquierdo != null || derecho.derecho != null)
-                {
-                    FacrorEquilibrio++;
-                }
-                derecho.CalcularFactorEq();
-            }
-            if(FacrorEquilibrio == 2 || FacrorEquilibrio == -2)
-            {
-                MessageBox.Show(clave.ToString() + ", " + FacrorEquilibrio.ToString());
-            }
-        }
         public bool RotacionRojoNegro()
         {
 
-            if(izquierdo != null)
+            if (izquierdo != null)
             {
-                if(izquierdo.color == Color.Red)
+                if (izquierdo.color == Color.DarkRed)
                 {
                     //Verificamos un posible Caso 1
                     bool dv = false;
                     if (derecho != null)
                     {
-                        if (derecho.color == Color.Red)
+                        if (derecho.color == Color.DarkRed)
                         {
                             dv = true;
                             if (izquierdo.izquierdo != null)
                             {
-                                if (izquierdo.izquierdo.color == Color.Red)
+                                if (izquierdo.izquierdo.color == Color.DarkRed)
                                 {
                                     //Caso1 Izquierda-Izquierda
-                                    color = Color.Red;
+                                    color = Color.DarkRed;
                                     izquierdo.color = Color.Gray;
                                     derecho.color = Color.Gray;
                                     return true;
@@ -185,10 +177,10 @@ namespace BlackRedTree
                             }
                             if (izquierdo.derecho != null)
                             {
-                                if (izquierdo.derecho.color == Color.Red)
+                                if (izquierdo.derecho.color == Color.DarkRed)
                                 {
                                     //Caso1 Izquierda-Derecha
-                                    color = Color.Red;
+                                    color = Color.DarkRed;
                                     izquierdo.color = Color.Gray;
                                     derecho.color = Color.Gray;
                                     return true;
@@ -196,15 +188,14 @@ namespace BlackRedTree
                             }
                         }
                     }
-                    if(dv != true)
+                    if (dv != true)
                     {
-                    //Verificamos un posible caso 2
+                        //Verificamos un posible caso 2
 
                         if (izquierdo.derecho != null)
                         {
-                            if (izquierdo.derecho.color == Color.Red)
+                            if (izquierdo.derecho.color == Color.DarkRed)
                             {
-                                MessageBox.Show("Caso 2");
                                 //Caso2 Izquierda(Se convierte a un caso 3 de izquierda)
                                 Nodo temp = izquierdo.derecho.izquierdo;
                                 izquierdo.derecho.izquierdo = izquierdo;
@@ -213,14 +204,13 @@ namespace BlackRedTree
                                 return true;
                             }
                         }
-                    //Verificamos un posible caso 3
-                        
-                        if(izquierdo.izquierdo != null)
+                        //Verificamos un posible caso 3
+
+                        if (izquierdo.izquierdo != null)
                         {
-                            if(izquierdo.izquierdo.color == Color.Red)
+                            if (izquierdo.izquierdo.color == Color.DarkRed)
                             {
-                                MessageBox.Show("Caso 3");
-                                Nodo temp = new Nodo(clave, Color.Red);
+                                Nodo temp = new Nodo(clave, Color.DarkRed);
                                 temp.izquierdo = izquierdo.derecho;
                                 temp.derecho = derecho;
                                 clave = izquierdo.clave;
@@ -237,21 +227,21 @@ namespace BlackRedTree
             }
             if (derecho != null)
             {
-                if (derecho.color == Color.Red)
+                if (derecho.color == Color.DarkRed)
                 {
                     //Verificamos un posible Caso 1
                     bool dv = false;
                     if (izquierdo != null)
                     {
-                        if (izquierdo.color == Color.Red)
+                        if (izquierdo.color == Color.DarkRed)
                         {
                             dv = true;
                             if (derecho.derecho != null)
                             {
-                                if (derecho.derecho.color == Color.Red)
+                                if (derecho.derecho.color == Color.DarkRed)
                                 {
                                     //Caso1 Izquierda-Izquierda
-                                    color = Color.Red;
+                                    color = Color.DarkRed;
                                     derecho.color = Color.Gray;
                                     izquierdo.color = Color.Gray;
                                     return true;
@@ -259,10 +249,10 @@ namespace BlackRedTree
                             }
                             if (derecho.izquierdo != null)
                             {
-                                if (derecho.izquierdo.color == Color.Red)
+                                if (derecho.izquierdo.color == Color.DarkRed)
                                 {
                                     //Caso1 Izquierda-Derecha
-                                    color = Color.Red;
+                                    color = Color.DarkRed;
                                     derecho.color = Color.Gray;
                                     izquierdo.color = Color.Gray;
                                     return true;
@@ -276,9 +266,8 @@ namespace BlackRedTree
 
                         if (derecho.izquierdo != null)
                         {
-                            if (derecho.izquierdo.color == Color.Red)
+                            if (derecho.izquierdo.color == Color.DarkRed)
                             {
-                                MessageBox.Show("Caso 2");
                                 //Caso2 Izquierda(Se convierte a un caso 3 de izquierda)
                                 Nodo temp = derecho.izquierdo.derecho;
                                 derecho.izquierdo.derecho = derecho;
@@ -291,10 +280,9 @@ namespace BlackRedTree
 
                         if (derecho.derecho != null)
                         {
-                            if (derecho.derecho.color == Color.Red)
+                            if (derecho.derecho.color == Color.DarkRed)
                             {
-                                MessageBox.Show("Caso 3");
-                                Nodo temp = new Nodo(clave, Color.Red);
+                                Nodo temp = new Nodo(clave, Color.DarkRed);
                                 temp.derecho = derecho.izquierdo;
                                 temp.izquierdo = izquierdo;
                                 clave = derecho.clave;
@@ -309,9 +297,6 @@ namespace BlackRedTree
 
                 }
             }
-
-
-
             //Caso3 Izquierda
 
             //Caso por default
@@ -321,44 +306,19 @@ namespace BlackRedTree
             }
             //Practicamos recursividad y retornamos resultados
             bool d = false;
-            if(derecho != null)
+            if (derecho != null)
             {
                 d = derecho.RotacionRojoNegro();
             }
-            if(izquierdo != null)
+            if (izquierdo != null)
             {
-                if(izquierdo.RotacionRojoNegro() == true || d == true)
+                if (izquierdo.RotacionRojoNegro() == true || d == true)
                 {
                     return true;
                 }
             }
             return false;
         }
-        /*
-         no es lo que queria pero devuelve el numero de nodos xd
-        public int altura(int altura_actual)
-        {
-            altura_actual++;
-            int a1 = 0 ;
-            if(derecho != null)
-            {
-                a1 = derecho.altura(altura_actual);
-            }
-            if(a1 > altura_actual)
-            {
-                altura_actual = a1;
-            }
-            if(izquierdo != null)
-            {
-                a1 = izquierdo.altura(altura_actual);
-            }
-            if (a1 > altura_actual)
-            {
-                altura_actual = a1;
-            }
-            return altura_actual;
-        }
-         */
 
     }
 }
